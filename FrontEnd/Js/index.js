3846489ccb206d9cb617f800a92c5793c94d27a7
Alexandre Detoc <alexandre.detoc@hotmail.fr>
2023-01-12 21:28:46 +0100

    ajout fonction suppression work

--- FrontEnd/Js/index.js
+++ FrontEnd/Js/index.js
@@ -23,12 +23,12 @@ let filtres = document.getElementById("filtres")
 
 const gallery = document.getElementById("gallery") 
 
-function displayWork(work) { 
+displayWork=(work) => { 
        
     gallery.innerHTML += `<figure>
                             <img crossorigin="anonymous" src="${work.imageUrl}" alt="${work.title}">
                             <figcaption>${work.title}</figcaption>
-                        </figure>`                        
+                        </figure>`                       
 }
 
 
@@ -59,11 +59,12 @@ function displayWork(work) {
             alert(error)
     })
     
- function displayCategorie(categorie) {
-    filtres.innerHTML += `<li><button data-id="${categorie.id}"> ${categorie.name}</button></li>`   
+displayCategorie=(categorie) => {
+    filtres.innerHTML += `<li><button data-id="${categorie.id}"> ${categorie.name}</button></li>`
+    optionCat.innerHTML += `<option>${categorie.name}</option>`   
 }
 
-function filtree(idCat) {
+filtree=(idCat) => {
     gallery.innerHTML = ""  
     for(let work of listWork){     
     console.log(work.categoryId) 
@@ -102,23 +103,33 @@ logOutBtn.addEventListener('click', () =>{
 
 // Modal
 
-const close = document.querySelector(".close")
+const closeBtn = document.querySelectorAll(".close")
+for (let i=0; i< closeBtn.length; i++){
+closeBtn[i].addEventListener('click', function() {
+    myModal.style.display = "none"
+  })
+}
 
-
-myBtnProjet.addEventListener('click', function() {
+myBtnProjet.addEventListener('click', () => {
     modalMain.innerHTML = ""
     myModal.style.display = "block";
     listWorks.style.display ='block'
     modalForm.style.display ='none'
     modalWorks(listWork)
 })
-function modalWorks(listWork){
+
+modalWorks=(listWork) => {
     for(let work of listWork) {
         displayWorkModal(work)
     }
-
+    let removeBtn = document.querySelectorAll(".fa-trash-can")
+        for(let i=0; i< removeBtn.length; i++){
+            removeBtn[i].addEventListener('click', () =>{
+            removeWork(listWork[i].id)
+            })}
 }
-function displayWorkModal(work) {
+
+displayWorkModal=(work) => {
     modalMain.innerHTML += `<figure>
                                  <img crossorigin="anonymous" src="${work.imageUrl}" alt="${work.title}">
                                  <figcaption>éditer</figcaption>
@@ -126,17 +137,44 @@ function displayWorkModal(work) {
                             </figure>`
 }
 
-
-
-close.addEventListener('click', function() {
-    myModal.style.display = "none"
-  })
-
 addWorks.addEventListener('click', function() {
     displayModalForm()
 })
 
-function displayModalForm(){
+displayModalForm=() => {
     listWorks.style.display ='none'
     modalForm.style.display ='flex'
 }
+
+
+removeWork =(id) => {
+    alert(id)
+    fetch('http://localhost:5678/api/works/' + id, {
+        method: 'DELETE',
+        headers: {
+            'accept' : '*/*',
+            'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY3MzUxODIwOSwiZXhwIjoxNjczNjA0NjA5fQ.vDQLbeAkMPbJOy-2iJenk34FFosn5KQTvJhhBgmenJ0',
+        }
+})
+.then (res => 
+        {
+         if(res.status === 200)
+        {
+         return res.json()
+         alert('suppression ok')
+        
+        }
+        else
+            {
+             if(res.status === 401){
+                  alert('Unauthorized')
+            }
+            else
+                {
+                 if(res.status === 404)
+                 {
+                    alert('Unexpected Behaviour')
+}}}})}
+                
+            
+    
